e287051c595b0b40dac758a3e6687b21449ec8fe
Kanishka <kanishkaudara2000@gmail.com>
2024-08-23 01:40:40 +0530

    Add exception on creating StarRating

---
+++ src/main/java/com/kanishka/rms/exception/IllegalStarRatingArgumentException.java
@@ -0,0 +1,7 @@
+package com.kanishka.rms.exception;
+
+public class IllegalStarRatingArgumentException extends Exception {
+    public IllegalStarRatingArgumentException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/kanishka/rms/model/StarRating.java
+++ src/main/java/com/kanishka/rms/model/StarRating.java
@@ -1,9 +1,14 @@
 package com.kanishka.rms.model;
 
+import com.kanishka.rms.exception.IllegalStarRatingArgumentException;
+
 public enum StarRating {
     STAR_1, STAR_2, STAR_3, STAR_4, STAR_5;
 
-    public static StarRating valueOf(int id) {
+    public static StarRating valueOf(int id) throws IllegalStarRatingArgumentException {
+        if(id < 1 || id > 5) {
+            throw new IllegalStarRatingArgumentException("Star rating argument is illegal: " + id);
+        }
         switch (id) {
             case 1 -> {
                 return STAR_1;
