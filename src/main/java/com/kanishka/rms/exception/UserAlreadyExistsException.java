e2324d52831eaa221a44450d338bedd3c022329c
Kanishka <kanishkaudara2000@gmail.com>
2024-08-25 19:21:21 +0530

    Add new exception

---
+++ src/main/java/com/kanishka/rms/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.kanishka.rms.exception;
+
+public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
