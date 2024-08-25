90d3ef86d7fc66af2da7f37aa4b0ede9f44f6cad
Kanishka <kanishkaudara2000@gmail.com>
2024-08-25 19:05:28 +0530

    Add DTO class

---
+++ src/main/java/com/kanishka/rms/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.kanishka.rms.dto;
+
+public class UserDTO {
+    private String fname;
+    private String lname;
+    private String username;
+    private String password;
+    private String usertype;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+}
