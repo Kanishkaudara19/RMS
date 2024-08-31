7671c82e5bbc1d6b40fad472fe1689da01d74780
Kanishka <kanishkaudara2000@gmail.com>
2024-08-31 12:52:04 +0530

    Add @Id mapping

--- src/main/java/com/kanishka/rms/entity/Contact.java
+++ src/main/java/com/kanishka/rms/entity/Contact.java
@@ -3,13 +3,16 @@ package com.kanishka.rms.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
 
 @Entity
 public class Contact {
     @Id
+    private Long id;
     @OneToOne
+    @MapsId
     @JoinColumn(name = "customer_user_id", nullable = false)
     private User customer;
     @Column(nullable = false)
