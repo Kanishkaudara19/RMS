7638e668a8852b2a02d94e786aaf197147b8c202
Kanishka <kanishkaudara2000@gmail.com>
2024-08-31 13:12:28 +0530

    Add food repository and service classes

---
+++ src/main/java/com/kanishka/rms/service/FoodService.java
@@ -0,0 +1,26 @@
+package com.kanishka.rms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kanishka.rms.dto.FoodDTO;
+import com.kanishka.rms.entity.Food;
+import com.kanishka.rms.repo.FoodRepository;
+
+public class FoodService {
+    @Autowired
+    private final FoodRepository foodRepository;
+
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    public void insert(FoodDTO foodDTO) {
+        Food food = new Food();
+        food.setName(foodDTO.getName());
+        food.setPrice(foodDTO.getPrice());
+        food.setAvailable(foodDTO.getAvailable());
+        food.setThumbnailUrl(foodDTO.getThumbnailUrl());
+
+        foodRepository.save(food);
+    }
+}
