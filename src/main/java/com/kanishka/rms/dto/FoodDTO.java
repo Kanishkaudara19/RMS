5e0a894d17e175a3b563828eb78be2a3613be911
Kanishka <kanishkaudara2000@gmail.com>
2024-08-31 13:10:25 +0530

    Add food dto class

---
+++ src/main/java/com/kanishka/rms/dto/FoodDTO.java
@@ -0,0 +1,49 @@
+package com.kanishka.rms.dto;
+
+public class FoodDTO {
+    private long id;
+    private String name;
+    private int price;
+    private int available;
+    private String thumbnailUrl;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
