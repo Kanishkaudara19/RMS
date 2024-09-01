37bbd965ea7b8d496a3e2216b62411996af2fd7c
Kanishka <kanishkaudara2000@gmail.com>
2024-09-01 20:29:41 +0530

    Add order repository

---
+++ src/main/java/com/kanishka/rms/repo/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.kanishka.rms.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kanishka.rms.entity.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
