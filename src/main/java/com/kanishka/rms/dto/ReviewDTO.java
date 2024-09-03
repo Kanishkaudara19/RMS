a43731fafc42c522450bfc654767a31dc753107b
Kanishka <kanishkaudara2000@gmail.com>
2024-09-04 02:59:50 +0530

    Add review dto class

---
+++ src/main/java/com/kanishka/rms/dto/ReviewDTO.java
@@ -0,0 +1,31 @@
+package com.kanishka.rms.dto;
+
+public class ReviewDTO {
+    private String customerName;
+    private int rating;
+    private String comment;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
