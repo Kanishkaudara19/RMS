6c60f9271360e8871959e0ba709157ff7b7aafaf
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 00:09:42 +0530

    Add reservation abort exception class

---
+++ src/main/java/com/kanishka/rms/exception/ReservationAbortException.java
@@ -0,0 +1,7 @@
+package com.kanishka.rms.exception;
+
+public class ReservationAbortException extends Exception {
+    public ReservationAbortException(String message) {
+        super(message);
+    }
+}
