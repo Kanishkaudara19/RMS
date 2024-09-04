8c6014e8a58791c36e7b4b048bd9556f3195629c
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 01:16:18 +0530

    Add new exception

---
+++ src/main/java/com/kanishka/rms/exception/ContactNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kanishka.rms.exception;
+
+public class ContactNotFoundException extends Exception {
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+}
