294a239e8f8902c064abbf558528d8a7f45edad1
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 02:52:13 +0530

    Add new exception

---
+++ src/main/java/com/kanishka/rms/exception/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kanishka.rms.exception;
+
+public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
