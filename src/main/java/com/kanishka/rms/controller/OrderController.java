3186def45ac1ba6fc7e2ff35424723c19b2d2736
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 03:27:20 +0530

    Add new endpoint to update status of the order

--- src/main/java/com/kanishka/rms/controller/OrderController.java
+++ src/main/java/com/kanishka/rms/controller/OrderController.java
@@ -7,13 +7,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kanishka.rms.dto.DetailedOrderDTO;
 import com.kanishka.rms.dto.OrderDTO;
+import com.kanishka.rms.entity.Order;
 import com.kanishka.rms.entity.User;
 import com.kanishka.rms.exception.UserNotFoundException;
+import com.kanishka.rms.model.OrderType;
+import com.kanishka.rms.service.DeliveryService;
 import com.kanishka.rms.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -22,9 +26,12 @@ import jakarta.servlet.http.HttpServletRequest;
 public class OrderController {
     @Autowired
     private final OrderService orderService;
+    @Autowired
+    private final DeliveryService deliveryService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, DeliveryService deliveryService) {
         this.orderService = orderService;
+        this.deliveryService = deliveryService;
     }
 
     @PostMapping("/insert")
@@ -56,5 +63,31 @@ public class OrderController {
         } catch (Exception ignored) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("/update")
+    public ResponseEntity<String> updateStatus(@RequestParam("oid") String orderId,
+                                               @RequestParam("status") String status,
+                                               HttpServletRequest request) {
+        ResponseEntity<String> response;
+        User user = (User) request.getSession().getAttribute("user");
+
+        if(user == null) {
+            response = ResponseEntity.badRequest().body("User should be logged in");
+        } else {
+            try {
+                Order order = orderService.updateStatus(orderId, status);
+
+                if(order.getOrderType() == OrderType.DELIVERY && "Deliver".equals(status)) {
+                    deliveryService.insertDeliveryUser(user, order);
+                }
+
+                response = ResponseEntity.ok().body("Order status updated successfully!");
+            } catch (Exception ex) {
+                response = ResponseEntity.badRequest().body("Something went wrong! Please try again later.");
+            }
+        }
+
+        return response;
     }
 }
