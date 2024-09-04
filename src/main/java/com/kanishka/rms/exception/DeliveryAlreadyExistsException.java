7a49e9a7478d94e796f0938b775c8c42981a886b
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 03:16:29 +0530

    Add new exception

---
+++ src/main/java/com/kanishka/rms/exception/DeliveryAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.kanishka.rms.exception;
+
+public class DeliveryAlreadyExistsException extends Exception {
+    public DeliveryAlreadyExistsException(String message) {
+        super(message);
+    }
+}
