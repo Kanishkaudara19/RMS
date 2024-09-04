ba1dde2d7055258fd29a5f196afabd1c93f6eb48
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 01:49:27 +0530

    Add new method to get order food by the order

--- src/main/java/com/kanishka/rms/repo/OrderFoodRepository.java
+++ src/main/java/com/kanishka/rms/repo/OrderFoodRepository.java
@@ -1,8 +1,12 @@
 package com.kanishka.rms.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.kanishka.rms.entity.Order;
 import com.kanishka.rms.entity.OrderFood;
 
 public interface OrderFoodRepository extends JpaRepository<OrderFood, Long> {
+    List<OrderFood> findAllByOrder(Order order);
 }
