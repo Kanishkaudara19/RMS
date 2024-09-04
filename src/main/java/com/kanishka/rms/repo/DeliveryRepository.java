6ce3f1f402b316c0daa1a5c34202495ac47ee526
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 03:15:46 +0530

    Update delivery repository

--- src/main/java/com/kanishka/rms/repo/DeliveryRepository.java
+++ src/main/java/com/kanishka/rms/repo/DeliveryRepository.java
@@ -1,8 +1,12 @@
 package com.kanishka.rms.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kanishka.rms.entity.Delivery;
+import com.kanishka.rms.entity.Order;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+    Optional<Delivery> findDeliveryByOrder(Order order);
 }
