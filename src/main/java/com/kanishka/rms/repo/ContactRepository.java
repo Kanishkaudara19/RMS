6630e32a5869e186d53e942365ee4a6d05ffdfc0
Kanishka <kanishkaudara2000@gmail.com>
2024-09-04 19:14:32 +0530

    Add new method

--- src/main/java/com/kanishka/rms/repo/ContactRepository.java
+++ src/main/java/com/kanishka/rms/repo/ContactRepository.java
@@ -1,8 +1,12 @@
 package com.kanishka.rms.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kanishka.rms.entity.Contact;
+import com.kanishka.rms.entity.User;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Optional<Contact> findByCustomer(User user);
 }
