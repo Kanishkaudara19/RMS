2f61ddd371966ca846c2592e1ba484c27b0463a9
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 02:49:35 +0530

    Add new method to get order

--- src/main/java/com/kanishka/rms/repo/OrderRepository.java
+++ src/main/java/com/kanishka/rms/repo/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.kanishka.rms.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kanishka.rms.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderId(String orderId);
 }
