6c0ec32441c0782181bd346cbdd2dab4da56d6d4
Kanishka <kanishkaudara2000@gmail.com>
2024-09-05 11:36:20 +0530

    Add report dto class

---
+++ src/main/java/com/kanishka/rms/dto/ReportDTO.java
@@ -0,0 +1,124 @@
+package com.kanishka.rms.dto;
+
+public class ReportDTO {
+    private int totalIncome;
+    private int totalOrders;
+    private int totalReservations;
+    private int totalUsers;
+
+    private int weeklyIncome;
+    private int monthlyIncome;
+    private int yearlyIncome;
+
+    private int weeklyOrders;
+    private int monthlyOrders;
+    private int yearlyOrders;
+
+    private int weeklyReservations;
+    private int monthlyReservations;
+    private int yearlyReservations;
+
+    public int getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(int totalIncome) {
+        this.totalIncome = totalIncome;
+    }
+
+    public int getTotalOrders() {
+        return totalOrders;
+    }
+
+    public void setTotalOrders(int totalOrders) {
+        this.totalOrders = totalOrders;
+    }
+
+    public int getTotalReservations() {
+        return totalReservations;
+    }
+
+    public void setTotalReservations(int totalReservations) {
+        this.totalReservations = totalReservations;
+    }
+
+    public int getTotalUsers() {
+        return totalUsers;
+    }
+
+    public void setTotalUsers(int totalUsers) {
+        this.totalUsers = totalUsers;
+    }
+
+    public int getWeeklyIncome() {
+        return weeklyIncome;
+    }
+
+    public void setWeeklyIncome(int weeklyIncome) {
+        this.weeklyIncome = weeklyIncome;
+    }
+
+    public int getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(int monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public int getYearlyIncome() {
+        return yearlyIncome;
+    }
+
+    public void setYearlyIncome(int yearlyIncome) {
+        this.yearlyIncome = yearlyIncome;
+    }
+
+    public int getWeeklyOrders() {
+        return weeklyOrders;
+    }
+
+    public void setWeeklyOrders(int weeklyOrders) {
+        this.weeklyOrders = weeklyOrders;
+    }
+
+    public int getMonthlyOrders() {
+        return monthlyOrders;
+    }
+
+    public void setMonthlyOrders(int monthlyOrders) {
+        this.monthlyOrders = monthlyOrders;
+    }
+
+    public int getYearlyOrders() {
+        return yearlyOrders;
+    }
+
+    public void setYearlyOrders(int yearlyOrders) {
+        this.yearlyOrders = yearlyOrders;
+    }
+
+    public int getWeeklyReservations() {
+        return weeklyReservations;
+    }
+
+    public void setWeeklyReservations(int weeklyReservations) {
+        this.weeklyReservations = weeklyReservations;
+    }
+
+    public int getMonthlyReservations() {
+        return monthlyReservations;
+    }
+
+    public void setMonthlyReservations(int monthlyReservations) {
+        this.monthlyReservations = monthlyReservations;
+    }
+
+    public int getYearlyReservations() {
+        return yearlyReservations;
+    }
+
+    public void setYearlyReservations(int yearlyReservations) {
+        this.yearlyReservations = yearlyReservations;
+    }
+}
